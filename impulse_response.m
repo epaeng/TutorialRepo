ab2cdb11e3e53bf969ae9b2286f4ef29907ac629
epaeng <epaeng@hmc.edu>
2016-01-19 17:46:33 -0800

    Fixed code in impulse_response.m, removed unnecessary file Qfcn.m

--- impulse_response.m
+++ impulse_response.m
@@ -5,11 +5,12 @@ function [ h,t ] = impulse_response( x, y, fs )
 %           y, measured output signal
 %           fs, sampling frequency of measurements
 %   outputs:    h, calculated impulse response
+%				t, time vector
 
 [X,f] = fdomain(x,fs);
 [Y,f] = fdomain(y,fs);
 H = X./Y;   % the calculated frequency response
 [h,t] = tdomain(H,fs);
-
+plot(t, h);	% plots impulse response at time
 end
 
